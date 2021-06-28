cfa6098558ffaa8dd56975b408f5d742550c5629
yeisoncr <yeisoncr@ufps.edu.co>
2021-06-28 08:46:47 -0500

    inicio

---
+++ src/main/java/com/tiendaapi/ExamenWeb20211Application.java
@@ -0,0 +1,13 @@
+package com.tiendaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenWeb20211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenWeb20211Application.class, args);
+	}
+
+}
